afb2abba18c3a9686e497c87b25315a1d27c6f28
Jorge Oscar Gianotti <jorge.oscar.gianotti@gmail.com>
2023-04-29 06:52:48 -0300

    feat: add api authenticator

---
+++ src/Security/ApiAuthenticator.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\SecurityRequestAttributes;
+
+class ApiAuthenticator extends AbstractAuthenticator
+{
+    const LOGIN_ROUTE = 'app_login';
+
+    public function __construct(private UrlGeneratorInterface $urlGenerator)
+    {
+    }
+
+    public function supports(Request $request): ?bool
+    {
+        return $request->isMethod('POST') && $this->getLoginUrl($request) === $request->getBaseUrl(
+            ) . $request->getPathInfo();
+    }
+
+    public function authenticate(Request $request): Passport
+    {
+        $email = $request->request->get('email', '');
+
+        $request->getSession()->set(Security::LAST_USERNAME, $email);
+
+        return new Passport(
+            new UserBadge($email),
+            new PasswordCredentials($request->request->get('password', ''))
+        );
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        return new JsonResponse(
+            data: [
+                'user' => $token->getUserIdentifier(),
+                'roles' => $token->getRoleNames()
+            ],
+            status: Response::HTTP_OK
+        );
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
+    {
+        if ($request->hasSession()) {
+            $request->getSession()->set(SecurityRequestAttributes::AUTHENTICATION_ERROR, $exception);
+        }
+
+        return new JsonResponse(status: Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function start(Request $request, AuthenticationException $authException = null): Response
+    {
+        /**
+         * If you would like this class to control what happens when an anonymous user accesses a
+         * protected page (e.g. redirect to /login), uncomment this method and make this class
+         * implement Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface.
+         *
+         * For more details, see https://symfony.com/doc/current/security/experimental_authenticators.html#configuring-the-authentication-entry-point
+         */
+        return new JsonResponse(status: Response::HTTP_UNAUTHORIZED);
+    }
+
+    protected function getLoginUrl(Request $request): string
+    {
+        return $this->urlGenerator->generate(self::LOGIN_ROUTE);
+    }
+}
